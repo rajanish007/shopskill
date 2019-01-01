ff24527994b38e634c5e729e85e68cb704573f10
rajanish007 <ranjan.rajanish0@gmail.com>
2019-01-02 02:15:58 +0530

    initial commit
    Signed-off-by: rajanish007 <ranjan.rajanish0@gmail.com>

---
+++ src/main/java/com/epex/shopskill/ShopskillApplication.java
@@ -0,0 +1,14 @@
+package com.epex.shopskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopskillApplication.class, args);
+	}
+
+}
+
